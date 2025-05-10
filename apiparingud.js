343b48db1a713cc6aa05d1279c98e99889113e42
EK023 <144771846+EK023@users.noreply.github.com>
2025-05-10 23:31:00 +0300

    Update apiparingud.js

--- apiparingud.js
+++ apiparingud.js
@@ -99,12 +99,12 @@ async function handleFormSubmit(event) {
 }
 
 async function listiraamatud() {
-    const responseData = await getDataAsJson("http://localhost:5000/raamatud/");
+    const responseData = await getDataAsJson("http://hs9-flask-api-raamatud-emfngyejhddfc0ev.northeurope-01.azurewebsites.net/raamatud/");
     const resultElement = document.getElementById("raamatud_result");
     resultElement.innerHTML = ""
     for (var raamat of responseData.raamatud){
-            resultElement.innerHTML += '<a href="http://localhost:5000/raamatud/'+raamat+'"  download="'+raamat+'.txt" >' +raamat+".txt</a> " +
-                        '<a href="#" onclick="deleteObject(\'http://localhost:5000/raamatud/'+raamat+'\')" > [kustuta]</a>' +
+            resultElement.innerHTML += '<a href="http://hs9-flask-api-raamatud-emfngyejhddfc0ev.northeurope-01.azurewebsites.net/raamatud/'+raamat+'"  download="'+raamat+'.txt" >' +raamat+".txt</a> " +
+                        '<a href="#" onclick="deleteObject(\'http://hs9-flask-api-raamatud-emfngyejhddfc0ev.northeurope-01.azurewebsites.net/raamatud/'+raamat+'\')" > [kustuta]</a>' +
                         "<br />";
     }
 
